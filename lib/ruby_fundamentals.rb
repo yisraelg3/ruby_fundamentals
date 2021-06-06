77773a44f7f8a585cbed538e0f52149e842fb1af
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-05 23:52:26 -0700

    initial commit

---
+++ lib/ruby_fundamentals.rb
@@ -0,0 +1,6 @@
+require "ruby_fundamentals/version"
+
+module RubyFundamentals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruby_fundamentals_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RubyFundamentals do
+  it "has a version number" do
+    expect(RubyFundamentals::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
