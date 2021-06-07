4227fcef6469e595b6b649deda8bd9ac842fc8d4
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-07 07:57:01 -0700

    start of lesson

--- lib/ruby_fundamentals.rb
+++ lib/ruby_fundamentals.rb
@@ -1,4 +1,6 @@
 require "ruby_fundamentals/version"
+require "ruby_fundamentals/friends"
+require "ruby_fundamentals/accessing_data"
 
 module RubyFundamentals
   class Error < StandardError; end
--- spec/ruby_fundamentals_spec.rb
+++ spec/ruby_fundamentals_spec.rb
@@ -2,8 +2,34 @@ RSpec.describe RubyFundamentals do
   it "has a version number" do
     expect(RubyFundamentals::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe "get_episodes" do 
+  it "takes a season number as an argument and returns an array of that season's episodes" do 
+    expect(false).to be true
   end
+end
+
+RSpec.describe "who_played" do 
+  it "takes a character name as an argument and returns a hash describing the actor who played that character" do 
+    expect(false).to be true
+  end
+end
+
+RSpec.describe "get_episode_name" do 
+  it "takes a season number and episode number as an argument and returns the name of the episode" do
+    expect(false).to be true
+  end 
+end
+
+RSpec.describe "get_episode_names" do 
+  it "returns an array of episode names" do 
+    expect(false).to be true
+  end
+end
+
+Rspec.describe "get_episode_by_name" do 
+  it "takes an episode name as an argument and returns a hash of data about that episode" do 
+    expect(false).to be true
+  end 
 end
